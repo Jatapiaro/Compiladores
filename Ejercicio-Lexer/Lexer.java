d6e25e6e39a15521dd6f181313c8961867069629
Jacobo <jacob.et.cetera@gmail.com>
2018-02-01 14:26:36 -0600

    Resuelve commentarios

--- Ejercicio-Lexer/Lexer.java
+++ Ejercicio-Lexer/Lexer.java
@@ -5,7 +5,8 @@ public class Lexer {
 
 	public int line = 1; 
 	private char peek = ' '; 
-	private Hashtable words = new Hashtable(); 
+	private Hashtable words = new Hashtable();
+	private boolean waitinForCommentaryEnd = false; 
 
 	public Lexer() { 
 		reserve( new Word(Tag.TRUE, "true") ); 
@@ -18,19 +19,54 @@ public class Lexer {
 
 	public Token scan() throws IOException { 
 		for( ; ; peek = (char)System.in.read() ) {
-			if( peek == ' ' || peek == '\t' ) continue; 
-			else if( peek == '\n' ) line = line + 1; 
-			else break; 
+			if ( peek == ' ' || peek == '\t' ) {
+				continue;
+			} 
+			else if ( peek == '\n' ) {
+				line = line + 1; 
+			} else {
+				break;
+			} 
 		}
+
+
+		if ( peek == '/' ) {
+			StringBuilder mySb = new StringBuilder();
+			System.in.mark(2);
+			char peek2 = (char)System.in.read();
+			if ( peek2 == '*') {
+				peek = (char)System.in.read();
+				while ( true ) {
+					peek = (char)System.in.read();
+					if ( peek == '*' ) {
+						peek = (char)System.in.read();
+						if ( peek == '/' ) {
+						    System.out.println("Tu comment fue: "+mySb.toString());
+						    break;
+						} else {
+							mySb.append("*");
+						}
+					} else {
+						mySb.append(peek);
+					} 
+				}
+			} else {
+				System.in.reset();
+				System.in.mark(2);
+			}
+		} 
+
 		if( Character.isDigit(peek) ) { 
 			int v = 0; 
 			do { 
 				v = 10*v + Character.digit(peek, 10); 
 				peek = (char)System.in.read(); 
 			} while( Character.isDigit(peek) ); 
-			return new Num(v); 
+			return new Num(v);
 		} 
+
 		if( Character.isLetter(peek) ) { 
+
 			StringBuffer b = new StringBuffer(); 
 			do { 
 				b.append(peek); 
@@ -46,6 +82,7 @@ public class Lexer {
 			w = new Word(Tag.ID, s); 
 			words.put(s, w); 
 			return w; 
+
 		} 
 		Token t = new Token(peek);
 		peek = ' ';
