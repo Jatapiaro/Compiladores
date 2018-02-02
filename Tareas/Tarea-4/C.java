e3906b456b908a60664cb4aaabb34333146a26e2
Jacobo <jacob.et.cetera@gmail.com>
2018-02-01 18:52:36 -0600

    Añade tarea 4

---
+++ Tareas/Tarea-4/C.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.IOException;
+
+/*
+* S -> 0S1 | 01
+*/
+
+public class C {
+
+	char current;
+	
+	public void readChar() throws IOException {
+		current = System.in.read();
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		C c = new C();
+	}
+
+}
