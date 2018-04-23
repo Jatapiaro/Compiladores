d7c82cf0c23428379826ba9bd2ffc644461c8929
Jacobo <jacob.et.cetera@gmail.com>
2018-04-23 02:01:41 -0500

    Añade update

--- Proyecto/Parser/SqlConnector.java
+++ Proyecto/Parser/SqlConnector.java
@@ -43,16 +43,27 @@ public class SqlConnector {
     //
     //=====================================================================
     public boolean insertTuple( Query q ) {
-        Statement stmt = null;
+        PreparedStatement stmt = null;
         ResultSet rs = null;
+        List<String> columns = listOfColumns(q.table);
+        List<String> primaryColumns = listOfPrimaryColumns(q.table);
         try {
-            stmt = con.createStatement();
-            String query = insertQueryString(q);
-            int affectedRows = stmt.executeUpdate(query);
+            String query = insertQueryString(q, columns, primaryColumns);
+            stmt = con.prepareStatement(query);
+            int index = 1;
+            for ( String value : q.values ) {
+                if (isParsable(value)) {
+                    stmt.setInt(index, Integer.parseInt(value));
+                } else {
+                    stmt.setString(index, value);
+                }
+                index++;
+            }
+            int affectedRows = stmt.executeUpdate();
             q.sqlQuery = query;
             q.function = "insert";
             List<String> result = new ArrayList<String>();
-            result.add("Created: "+affectedRows+" rows");
+            result.add("Created or modified: "+affectedRows+" rows");
             q.setQueryResult(result);
             return true;
         } catch(Exception e) {
@@ -61,21 +72,53 @@ public class SqlConnector {
         }
     }
 
-    public String insertQueryString( Query q ) {
-        StringBuilder query = new StringBuilder("INSERT INTO ");
-        query.append(q.table);
-        query.append(" VALUES(");
-        for ( String s : q.values ) {
-            if (isParsable(s)) {
-                query.append(Integer.parseInt(s)+",");
+
+    public String insertQueryString( Query q, List<String> columns, List<String> primaryColumns ) {
+        StringBuilder sb = new StringBuilder();
+        sb.append( "MERGE INTO "+q.table+" AS t USING (SELECT " );
+        StringBuilder putParams = new StringBuilder();
+        for ( int i = 0; i<columns.size(); i++ ) {
+            if ( i == columns.size()-1 ) {
+                putParams.append(columns.get(i)+" = ? ) AS s ");
             } else {
-                query.append("'"+s+"',");
+                putParams.append(columns.get(i)+" = ?, ");
             }
         }
-        query.append(")");
-        String s = query.reverse().toString().replaceFirst(",", "");
-        s = new StringBuilder(s).reverse().toString();
-        return s;
+        sb.append(putParams.toString());
+        StringBuilder keyColumns = new StringBuilder("ON ");
+        for ( int i = 0; i<primaryColumns.size(); i++ ) {
+            if ( i == primaryColumns.size()-1 ) {
+                keyColumns.append("t."+primaryColumns.get(i)+" = s."+primaryColumns.get(i)+" ");
+            } else {
+                keyColumns.append("t."+primaryColumns.get(i)+" = s."+primaryColumns.get(i)+" AND ");
+            }
+        }
+        sb.append(keyColumns.toString());
+        sb.append("WHEN MATCHED THEN ");
+        StringBuilder updateParameters = new StringBuilder("UPDATE SET ");
+        for ( int i = 1; i<columns.size(); i++ ) {
+            if ( i == columns.size()-1 ) {
+                updateParameters.append(columns.get(i)+"=s."+columns.get(i)+" ");
+            } else {
+                updateParameters.append(columns.get(i)+"=s."+columns.get(i)+", ");
+            }
+        }
+        sb.append(updateParameters.toString());
+        sb.append("WHEN NOT MATCHED THEN ");
+        StringBuilder insertColumns = new StringBuilder("( ");
+        StringBuilder insertValues = new StringBuilder("( ");
+        for ( int i = 0; i<columns.size(); i++ ) {
+            if ( i == columns.size()-1 ) {
+                insertColumns.append(columns.get(i)+" ) ");
+                insertValues.append("s."+columns.get(i)+" ) ");
+            } else {
+                insertColumns.append(columns.get(i)+", ");
+                insertValues.append("s."+columns.get(i)+", ");
+            }
+        }
+        sb.append("INSERT "+insertColumns.toString());
+        sb.append("VALUES "+insertValues.toString()+";");
+        return sb.toString();
     }
 
     //=====================================================================
@@ -159,7 +202,8 @@ public class SqlConnector {
         PreparedStatement stmt = null;
         ResultSet rs = null;
         try {
-            stmt = con.prepareStatement(retractQueryString(q, columns));
+            String query = retractQueryString(q, columns);
+            stmt = con.prepareStatement(query);
             int index = 1;
             for ( String value : q.values ) {
                 if (isParsable(value)) {
@@ -169,7 +213,12 @@ public class SqlConnector {
                 }
                 index++;
             }
-            return (stmt.executeUpdate() > 0)? true : false;
+            int queryResult = stmt.executeUpdate();
+            List<String> result = new ArrayList<String>();
+            result.add("Deleted "+queryResult+" rows");
+            q.sqlQuery = query;
+            q.setQueryResult(result);
+            return (queryResult > 0)? true : false;
         } catch(Exception e) {
             System.out.println(e);
             return true;
@@ -203,6 +252,27 @@ public class SqlConnector {
         StringBuilder q = new StringBuilder();
         q.append("SELECT COLUMN_NAME FROM "+databaseName+".INFORMATION_SCHEMA.COLUMNS");
         q.append(" WHERE TABLE_NAME = N'"+table+"'");
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(q.toString());
+            while (rs.next()) {
+                results.add(rs.getString(1));
+            }
+            return results;
+        } catch(Exception e) {
+            System.out.println(e);
+            return new ArrayList<String>();
+        }
+    }
+
+    public List<String> listOfPrimaryColumns(String table) {
+        List<String> results = new ArrayList<String>();
+        StringBuilder q = new StringBuilder();
+        q.append("SELECT COLUMN_NAME FROM "+databaseName+".INFORMATION_SCHEMA.KEY_COLUMN_USAGE");
+        q.append(" WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_SCHEMA + '.' + QUOTENAME(CONSTRAINT_NAME)), 'IsPrimaryKey') = 1 ");
+        q.append(" AND TABLE_NAME = '"+table+"'");
         Statement stmt = null;
         ResultSet rs = null;
         try {
